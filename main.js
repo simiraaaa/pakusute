6eecb0bdf1064d6efb23e78f0872cc8b3339fb12
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2015-11-03 21:17:56 +0900

    fix screen rotate and point converting

--- main.js
+++ main.js
@@ -43,10 +43,39 @@
     none: 0.2,
   };
   var settings = {
+    rotate: 'left', //どこがした
+    getTransformRotate: function() {
+      return this._rotateMap[this.rotate];
+    },
+    getPoint: function(x, y, w, h) { return { x: x, y: y }; },
+    getPointMap: {
+      left: function(x, y, w, h) {
+        return { y: h - x, x: y };
+      },
+      right: function(x, y, w, h) {
+        return { y: x, x: w - y };
+      },
+      bottom: function(x, y, w, h) {
+        return { x: x, y: y };
+      },
+      top: function(x, y, w, h) {
+        return { x: w - x, y: h - y };
+      }
+    },
+
+    _rotateMap: {
+      left: 'rotate(90deg)',
+      right: 'rotate(270deg)',
+      top: 'rotate(180deg)',
+      bottom: 'rotate(0deg)',
+    },
     refresh: function() {
       sounds.bgm.volume = this.volumes.bgm;
       sounds.se.volume = this.volumes.se;
       sounds.snare.volume = this.volumes.se;
+
+      this.getPoint = this.getPointMap[this.rotate];
+      app._fitFunc();
     },
     volumes: {
       se: 0.5,
@@ -100,17 +129,77 @@
   var SCORE = { "_order": ["D", "F", "J", "K","L"],L:[], "D": [4.672000000000001, 5.311999999999999, 10.360000000000001, 10.701333333333334, 11.010666666666667, 22.730666666666664, 23.461333333333332, 24.032, 25.050666666666665, 25.72, 27.072, 31.312, 34.61066666666667, 38.552, 39.90133333333333, 44.752, 44.94133333333333, 45.141333333333336, 45.49066666666667, 45.68, 45.800000000000004, 47.62133333333333, 47.98133333333334, 48.352000000000004, 48.68, 49.120000000000005, 49.792, 51.832, 52.53066666666667, 52.93066666666667, 53.072, 53.45066666666667, 53.6, 53.992000000000004, 54.141333333333336, 54.512, 55.800000000000004, 56.41066666666667, 57.080000000000005, 57.69066666666667, 58.312000000000005, 58.992000000000004, 59.36, 59.89066666666667, 60.432, 61.080000000000005, 61.730666666666664, 62.38133333333333, 63.032, 63.66133333333333, 64.30133333333333, 70.12, 70.52, 71.09066666666666, 71.272, 74.04, 74.72, 75.37066666666666, 76.02133333333333, 76.34133333333334], "F": [1.04, 3.621333333333333, 4.68, 6.352, 6.701333333333334, 6.9013333333333335, 7.181333333333333, 7.3919999999999995, 7.9013333333333335, 8.170666666666667, 8.592, 11.701333333333334, 12.330666666666668, 12.992, 13.610666666666667, 16.18133333333333, 16.490666666666666, 17.090666666666664, 20.70133333333333, 21.151999999999997, 21.741333333333333, 22.050666666666665, 22.72, 23.421333333333333, 24.032, 24.68, 25.4, 26.06133333333333, 26.84, 29.479999999999997, 29.639999999999997, 29.810666666666666, 29.992, 30.381333333333334, 30.69066666666667, 31.010666666666662, 31.591999999999995, 31.981333333333335, 32.25066666666667, 32.53066666666667, 34.04, 35.501333333333335, 36.92, 38.2, 39.57066666666667, 42.792, 42.952000000000005, 43.120000000000005, 43.30133333333333, 43.45066666666667, 43.73066666666667, 43.992000000000004, 44.28, 44.57066666666667, 46.072, 46.221333333333334, 46.440000000000005, 46.632000000000005, 46.81066666666667, 47, 47.17066666666667, 47.56, 47.912, 48.26133333333333, 48.592, 48.800000000000004, 49.45066666666667, 50.17066666666667, 50.32, 50.552, 50.872, 51.032000000000004, 52.192, 54.330666666666666, 54.800000000000004, 56.832, 58.04, 60.760000000000005, 63.352000000000004, 64.74133333333333, 65.32000000000001, 65.912, 66.09066666666666, 66.4, 66.70133333333334, 67.02133333333333, 67.33066666666667, 67.66133333333333, 67.952, 68.28, 68.60000000000001, 68.93066666666667, 69.29066666666667, 69.56, 69.912, 71.552, 71.98133333333334, 72.32000000000001, 73, 73.62133333333334, 74.37066666666666, 75.032, 75.712, 76.19200000000001, 76.50133333333333], "J": [0.8906666666666667, 1.0613333333333332, 2.0613333333333332, 2.1919999999999997, 2.3813333333333335, 3.44, 3.7706666666666666, 4.690666666666667, 5.981333333333334, 6.1306666666666665, 11.381333333333334, 12.040000000000001, 12.68, 13.32, 14.000000000000002, 14.160000000000002, 14.690666666666667, 14.831999999999999, 15.341333333333333, 15.501333333333333, 16, 16.32, 16.631999999999998, 17.36, 18.08, 18.741333333333333, 19.38133333333333, 20.04, 20.712, 21.170666666666666, 21.752, 22.06133333333333, 22.712, 23.410666666666664, 24.050666666666665, 24.690666666666665, 25.38133333333333, 26.06133333333333, 26.671999999999997, 30.151999999999997, 30.84, 31.471999999999998, 32.112, 32.4, 32.821333333333335, 34.032000000000004, 35.330666666666666, 36.72, 38.04, 39.432, 40.74133333333334, 41.32, 42.032000000000004, 42.36, 42.712, 42.872, 43.04, 43.21066666666667, 43.36, 43.832, 44.38133333333334, 44.672000000000004, 44.84, 44.98133333333334, 45.141333333333336, 45.30133333333333, 45.472, 45.64, 45.792, 45.952000000000005, 46.120000000000005, 46.28, 46.58133333333333, 46.752, 47.080000000000005, 47.2, 47.480000000000004, 47.821333333333335, 48.21066666666667, 48.53066666666667, 48.781333333333336, 49.461333333333336, 50.13066666666667, 50.26133333333333, 50.81066666666667, 50.94133333333333, 51.501333333333335, 52.192, 54.32, 54.81066666666667, 56.832, 58.05066666666667, 60.77066666666667, 63.352000000000004, 64.68, 64.82133333333334, 65.26133333333334, 65.41066666666667, 65.84, 65.98133333333334, 66.50133333333333, 67.13066666666667, 67.77066666666667, 68.4, 69.08, 69.712, 71.47200000000001, 72.16, 74.06133333333334, 74.73066666666666, 75.38133333333333, 76.04, 76.37066666666666], "K": [4.68, 5.32, 6.4719999999999995, 7.0213333333333345, 7.730666666666666, 8.410666666666668, 9.112, 9.701333333333334, 10.360000000000001, 10.701333333333334, 11.021333333333335, 11.690666666666667, 12.32, 12.992, 13.621333333333334, 14.312, 14.991999999999999, 15.680000000000001, 16.831999999999997, 17.730666666666664, 18.36, 19.032, 19.70133333333333, 20.352, 22.4, 23.06133333333333, 23.730666666666664, 24.370666666666665, 25.050666666666665, 25.72, 26.410666666666664, 27.450666666666667, 28, 28.639999999999997, 29.272, 30.52, 31.16, 31.781333333333333, 33.461333333333336, 34.6, 35.970666666666666, 37.392, 37.69066666666667, 38.392, 39.74133333333334, 43.58133333333333, 44.101333333333336, 46.4, 46.90133333333333, 47.41066666666667, 47.752, 48.13066666666667, 48.472, 49.13066666666667, 49.800000000000004, 50.42133333333334, 51.141333333333336, 51.85066666666667, 52.541333333333334, 52.90133333333333, 53.05066666666667, 53.440000000000005, 53.61066666666667, 53.992000000000004, 54.141333333333336, 54.52, 55.800000000000004, 56.416000000000004, 57.101333333333336, 57.70133333333334, 58.32, 58.992000000000004, 59.36, 59.89066666666667, 60.440000000000005, 61.09066666666667, 61.72, 62.38133333333333, 63.032, 63.68000000000001, 64.30133333333333, 66.17066666666666, 66.8, 67.432, 68.08, 68.74133333333333, 69.392, 70.08, 70.53066666666668, 71.10133333333333, 71.30133333333333, 71.8, 72.45066666666666, 73, 73.632, 74.36, 75.04, 75.70133333333334, 76.232] };
 
 
+  var SCENE_ARGUMENTS = {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    query: '#world',
+    assets: ASSETS,
+    fit:false,
+  };
 
   phina.main(function () {
-    app = phina.display.CanvasApp({
-      query: '#world',
-      width: SCREEN_WIDTH,
-      height: SCREEN_HEIGHT
-    });
+    app = phina.display.CanvasApp(SCENE_ARGUMENTS);
     app.enableStats();
     //pointing = app.pointing;
     app.fps = 60;
     app.backgroundColor = '#55a';
+
+    var _fitFunc = app._fitFunc=function() {
+      var e = app.domElement;
+      var s = e.style;
+
+      s.position = "absolute";
+      s.margin = "auto";
+      s.left = "0px";
+      s.top = "0px";
+      s.bottom = "0px";
+      s.right = "0px";
+
+      s.transform = settings.getTransformRotate();
+
+      if ({ left: 1, right: 1 }[settings.rotate]) {
+        var rateWidth = e.width / window.innerHeight;
+        var rateHeight = e.height / window.innerWidth;
+        var rate = e.height / e.width;
+
+        if (rateWidth > rateHeight) {
+          s.width = Math.floor(innerHeight) + "px";
+          s.height = Math.floor(innerHeight * rate) + "px";
+        }
+        else {
+          s.width = Math.floor(innerWidth / rate) + "px";
+          s.height = Math.floor(innerWidth) + "px";
+        }
+
+      } else {
+        var rateWidth = e.width / window.innerWidth;
+        var rateHeight = e.height / window.innerHeight;
+        var rate = e.height / e.width;
+
+        if (rateWidth > rateHeight) {
+          s.width = Math.floor(innerWidth) + "px";
+          s.height = Math.floor(innerWidth * rate) + "px";
+        }
+        else {
+          s.width = Math.floor(innerHeight / rate) + "px";
+          s.height = Math.floor(innerHeight) + "px";
+        }
+      }
+    };
+
+    var fitScreen = function(isEver) {
+      isEver = isEver === undefined ? true : isEver;
+
+
+      // 一度実行しておく
+      _fitFunc();
+
+      // リサイズ時のリスナとして登録しておく
+      if (isEver) {
+        window.addEventListener("resize", _fitFunc, false);
+      }
+    };
+    fitScreen();
     var isSoundAvailable = false;
     var func;
     app.domElement.addEventListener('touchend', func = function (e) {
@@ -125,12 +214,8 @@
 
 
 
-    app.pushScene(phina.game.LoadingScene({
-      width: SCREEN_WIDTH,
-      height: SCREEN_HEIGHT,
-      assets: ASSETS,
-    }).on('exit', function() {
-      app.replaceScene(GameScene());
+    app.pushScene(phina.game.LoadingScene(SCENE_ARGUMENTS).on('exit', function() {
+      app.replaceScene(SettingScene());
     }));
     app.run();
   });
@@ -138,17 +223,39 @@
   var GameScene = phina.define('', {
     superClass: phina.display.CanvasScene,
 
-    init: function () {
-      this.superInit();
+    init: function() {
+      this.superInit(SCENE_ARGUMENTS);
       settings.refresh();
       var music = Music('bgm', SCORE);
 
-      KeyButton.SE = [sounds.se, sounds.snare, sounds.se, sounds.snare, sounds.se];
       this.addChild(KeyButtonManager().setup());
       this.addChild(music);
       this.addChild(MessageLayer());
     },
   });
+
+  var SettingScene = phina.define('', {
+    superClass: phina.display.CanvasScene,
+
+    init: function() {
+      this.superInit(SCENE_ARGUMENTS);
+      settings.refresh();
+
+      KeyButton.SE = [sounds.se, sounds.snare, sounds.se, sounds.snare, sounds.se];
+    },
+    onenter: function() {
+      this.app.replaceScene(GameScene());
+    }
+  });
+
+  //var PopupScene = phina.define('', {
+  //  superClass: phina.display.CanvasScene,
+
+  //  init: function(w,h) {
+  //    this.superInit({width:w,height:h}.$safe(SCENE_ARGUMENTS));
+      
+  //  },
+  //});
 
 
   var MessageLayer = phina.define('', {
@@ -172,7 +279,7 @@
         fill: 'white',
         baseline:'top',
       })
-      .setPosition(10, 10)
+      .setPosition(20, 20)
       .addChildTo(this);
       this.label.alpha = 0.3;
 
@@ -185,7 +292,7 @@
       + ',good:' + this.good
       + ',bad:' + this.bad
       + ',miss:' + this.miss
-      + '\nchange speed:cursor up↑ or down↓';
+      + '\nchange speed:cursor up↑ or down↓\nchange rotate cursor ← →';
     }
 
 
@@ -279,7 +386,27 @@
       var sel=this;
       window.addEventListener('keydown' , function (e) {
         var k = e.which;
-        if (k === 38) { sel.speed += 0.05; } else if (k === 40) { sel.speed -= 0.05; }
+        if (k === 38) {
+          sel.speed += 0.05;
+        } else if (k === 40) {
+          sel.speed -= 0.05;
+        } else if (k === 37) {
+          settings.rotate = ({
+            left: 'top',
+            top: 'right',
+            right: 'bottom',
+            bottom: 'left',
+          })[settings.rotate];
+          settings.refresh();
+        } else if (k === 39) {
+          settings.rotate = ({
+            left: 'bottom',
+            top: 'left',
+            right: 'top',
+            bottom: 'right',
+          })[settings.rotate];
+          settings.refresh();
+        }
       });
       this.__image = ScoreImage({ type: type }).canvas.domElement;
     },
@@ -655,16 +782,16 @@
 
   });
 
-  KeyButtonManager.getPoint = function (e) {
+  KeyButtonManager.getPoint = function(e) {
+
     var elm = app.domElement;
     var style = elm.style;
-
     var rect = elm.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     x *= elm.width / window.parseInt(style.width, 10);
     y *= elm.height / window.parseInt(style.height, 10);
-    return { x: x, y: y };
+    return settings.getPoint(x, y, elm.width,elm.height);
   };
 
 
