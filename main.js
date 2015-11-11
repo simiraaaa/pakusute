f5fb12cbf44a2fa7f5bf9e307315eaffaf023823
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2015-11-11 21:37:49 +0900

    test label speed

--- main.js
+++ main.js
@@ -6,10 +6,66 @@
 
   // ---------- define area -------
 
+  
+  // debug area-----------------
+  phina.display.Label.prototype.$extend({
+    
+    _render: function() {
+      var s = new Date;
+      var b =!Math.randint(0,60);
+      var canvas = this.canvas;
+      var context = canvas.context;
 
+      var fontSize = this.fontSize;
+      var font = "{fontWeight} {fontSize}px {fontFamily}".format(this);
+      var text = this.text + '';
+      var lines = this._lines = text.split('\n');
+      canvas.context.font = font;
+      if(b)console.log(new Date - s, 'font');
+      var w = this.calcWidth() + this.padding * 2;
+      var h = this.calcHeight() + this.padding * 2;
+      this._renderBackground(w, h);
 
+      if (b) console.log(new Date - s, 'bg');
+      canvas.transformCenter();
+      context.font = font;
+      context.textAlign = this.align;
+      context.textBaseline = this.baseline;
+
+      if (b) console.log(new Date - s, 'context');
+      var lineSize = fontSize * this.lineHeight;
+      var offset = -Math.floor(lines.length / 2) * lineSize;
+      offset += ((lines.length + 1) % 2) * (lineSize / 2);
+      if (b) console.log(new Date - s, 'st');
+      if (this.stroke) {
+        context.strokeStyle = this.stroke;
+        context.lineWidth = this.strokeWidth;
+        context.lineJoin = "round";
+        context.shadowBlur = 0;
+        lines.forEach(function(line, i) {
+          context.strokeText(line, 0, i * lineSize + offset);
+        }, this);
+      }
+      if (b) console.log(new Date - s, 's');
+      if (this.shadow) {
+        context.shadowColor = this.shadow;
+        context.shadowBlur = this.shadowBlur;
+      }
+
+      if (b) console.log(new Date - s, 'sh');
+      if (this.fill) {
+        context.fillStyle = this.fill;
+        lines.forEach(function(line, i) {
+          context.fillText(line, 0, i * lineSize + offset);
+        }, this);
+      }
+      if (b) console.log(new Date - s, 'r');
+    },
+  });
 
   // --------- setting -----------
+
+  
 
   // スケールと回転を考慮
   phina.input.Input.prototype._move = function(x, y) {
@@ -42,7 +98,7 @@
   描画の更新はUPDATEで行う。
 
   */
-  var VERSION = '0.0.14';
+  var VERSION = '0.0.15';
 
   var SCREEN_WIDTH = 480;
   var SCREEN_HEIGHT = 320;
